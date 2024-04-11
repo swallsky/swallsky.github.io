55b49e825c07589cf7257ec8a8255a6d6cc42819
jackXu <xujinzhang@372163.com>
2024-04-11 16:05:10 +0800

    fixed page

--- src/index/Page.tsx
+++ src/index/Page.tsx
@@ -5,7 +5,7 @@ function Page(props: any) {
   return (
     <Layout>
       <div>
-        <h3>码农魔法书</h3>
+        <h1>码农魔法书</h1>
         <div className="summary">拥有超能力，可以操控时间、空间、能量、信息技术文档！</div>
       </div>
     </Layout>
