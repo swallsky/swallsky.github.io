6581c055e0a25c113990b8efd8ae7c9e3bc9dc47
jackXu <xujinzhang@372163.com>
2024-04-11 16:00:54 +0800

    code surprise

--- src/index/Page.tsx
+++ src/index/Page.tsx
@@ -5,8 +5,8 @@ function Page(props: any) {
   return (
     <Layout>
       <div>
-        <h3>swallsky books</h3>
-        <div className="summary">技术文档分享</div>
+        <h3>码农魔法书</h3>
+        <div className="summary">拥有超能力，可以操控时间、空间、能量、信息技术文档！</div>
       </div>
     </Layout>
   );
